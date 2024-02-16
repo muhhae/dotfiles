e951d8afa20381aee508bfbcd84e33deb109dcc9
muhhae <haekal.muhamat@gmail.com>
2024-02-16 20:08:21 +0700

    script to symlink

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+DIR=$(dirname "$0")
+
+ln -sr  $DIR/ags $HOME/.config/.
+ln -sr  $DIR/hypr $HOME/.config/.
+ln -sr  $DIR/nvim $HOME/.config/.
+ln -sr  $DIR/swaync $HOME/.config/.
+ln -sr  $DIR/waybar $HOME/.config/.
+ln -sr  $DIR/wezterm $HOME/.config/.
+ln -sr  $DIR/xdg-desktop-portal $HOME/.config/.
+ln -sr  $DIR/wofi $HOME/.config/.
+ln -sr  $DIR/.zshrc $HOME/.
