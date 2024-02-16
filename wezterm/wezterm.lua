c0686df4d04604d49d64a115ffbe9a64634a4544
muhhae <haekal.muhamat@gmail.com>
2024-02-16 07:28:34 +0700

    setup wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Tokyo Night Moon'
+config.font = wezterm.font_with_fallback {
+  'JetBrainsMono Nerd Font',
+  'Fira Code Nerd Font',
+}
+config.font_size = 14
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
