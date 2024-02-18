f232c1af86315018f8b510cd2e595bfc6c7538df
muhhae <haekal.muhamat@gmail.com>
2024-02-18 09:15:26 +0700

    tweaks padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,8 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night Moon'
 config.font = wezterm.font_with_fallback {
@@ -17,7 +15,7 @@ config.window_padding = {
   left = 8,
   right = 8,
   top = 8,
-  bottom =8,
+  bottom = 8,
 }
 config.window_close_confirmation = 'NeverPrompt'
 return config
