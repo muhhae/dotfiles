fb7bbfdd2c02506c7cf7ed7424eacfd08ed02e56
muhhae <haekal.muhamat@gmail.com>
2024-02-22 18:42:08 +0700

    nord colorscheme

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,2 +1,4 @@
-vim.cmd("colorscheme tokyonight-moon")
+-- vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("colorscheme nord")
 -- vim.cmd [[highlight Normal guibg=none]]
+vim.cmd('highlight EndOfBuffer guibg=bg guifg=bg')
--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,6 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+    use 'nordtheme/vim'
     use 'karb94/neoscroll.nvim'
     use "folke/persistence.nvim"
     use 'andweeb/presence.nvim'
