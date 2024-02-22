3c549bc8097f4e380aa58f93922cfccd13641d6a
muhhae <haekal.muhamat@gmail.com>
2024-02-22 08:31:06 +0700

    nothing just random thing that i forgot

--- nvim/after/plugin/presence.lua
+++ nvim/after/plugin/presence.lua
@@ -1,7 +1,5 @@
 local presence = require 'presence'
 
-
-
 presence.setup({
     auto_update         = true,
     -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
@@ -23,23 +21,12 @@ presence.setup({
         templ = { "Templ", "https://cdn.icon-icons.com/icons2/2699/PNG/512/golang_logo_icon_171073.png" },
         go = { "Go", "https://cdn.icon-icons.com/icons2/2699/PNG/512/golang_logo_icon_171073.png" }
     },
-    -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
     show_time           = true,
-    -- Show the timer
-
-    -- Rich Presence text options
     editing_text        = "Messing with %s",
-    -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
     file_explorer_text  = "Looking for a file to mess with",
-    -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
     git_commit_text     = "Committing some crime (changes)",
-    -- Format string rendered when committing changes in git (either string or function(filename: string): string)
     plugin_manager_text = "Managing plugins",
-    -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
     reading_text        = "Reading %s",
-    -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
     workspace_text      = "Working on %s",
-    -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
     line_number_text    = "Line %s out of %s",
-    -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
 })
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,17 +5,19 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night Moon'
+
 config.font = wezterm.font_with_fallback {
-  'JetBrainsMono Nerd Font',
-  'Fira Code Nerd Font',
+    'Fira Code Nerd Font',
+    -- 'JetBrainsMono Nerd Font',
 }
+
 config.font_size = 14
 config.enable_tab_bar = false
 config.window_padding = {
-  left = 8,
-  right = 8,
-  top = 8,
-  bottom = 8,
+    left = 8,
+    right = 8,
+    top = 8,
+    bottom = 8,
 }
 config.window_close_confirmation = 'NeverPrompt'
 return config
