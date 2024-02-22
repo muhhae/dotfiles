c3e54d3a71e8fbc99af30d321e7aa96701a5f5fa
muhhae <haekal.muhamat@gmail.com>
2024-02-22 20:46:23 +0700

    hypr border color and nvim hyprlang

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,6 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+    use "luckasRanarison/tree-sitter-hyprlang"
     use 'nordtheme/vim'
     use 'karb94/neoscroll.nvim'
     use "folke/persistence.nvim"
