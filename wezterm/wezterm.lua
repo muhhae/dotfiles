28146a3d1edb340bbf5293170f82bb9263496cfa
muhhae <haekal.muhamat@gmail.com>
2024-02-23 18:07:43 +0700

    use wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.color_scheme = 'nord'
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.7
 
 config.font = wezterm.font_with_fallback {
     'JetBrainsMono Nerd Font',
@@ -11,10 +11,10 @@ config.font = wezterm.font_with_fallback {
 config.font_size = 15
 config.enable_tab_bar = false
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+    left = 6,
+    right = 6,
+    top = 6,
+    bottom = 6,
 }
 
 config.window_close_confirmation = 'NeverPrompt'
