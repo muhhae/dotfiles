13bedca28d4743a77d6268d8119aeca9d055adfb
muhhae <haekal.muhamat@gmail.com>
2024-02-23 10:11:09 +0700

    hiding tilde everytime new buffer opened on window.

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -25,3 +25,13 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     callback = custom_format,
     group = autocmd_group,
 })
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+    desc = "hide tilde ~",
+    callback = function()
+        print("Hiding tilde ~, go to autocmd.lua to disable")
+        vim.wo.fillchars = 'eob: '
+        vim.cmd('highlight SignColumn guibg=transparent')
+    end,
+    group = autocmd_group,
+})
