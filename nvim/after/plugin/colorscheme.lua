cb7b7bc2f47ad2ac3dae25164b04024d8760e580
muhhae <haekal.muhamat@gmail.com>
2024-02-23 10:11:09 +0700

    hiding tilde everytime new buffer opened on window.

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,6 +1,6 @@
 -- vim.cmd("colorscheme tokyonight-moon")
 vim.cmd("colorscheme nord")
 vim.cmd [[highlight Normal guibg=none]]
--- vim.cmd('highlight EndOfBuffer guibg=bg guifg=bg')
+
 vim.wo.fillchars = 'eob: '
 vim.cmd('highlight SignColumn guibg=transparent')
