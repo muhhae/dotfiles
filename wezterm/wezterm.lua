e28e482a28fb064e99631dd981c5cd4b3d4364b8
muhhae <haekal.muhamat@gmail.com>
2024-02-23 10:10:44 +0700

    setup wezterm nord transparent etc

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,23 +1,21 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
--- This will hold the configuration.
 local config = wezterm.config_builder()
--- This is where you actually apply your config choices
--- For example, changing the color scheme:
-config.color_scheme = 'Tokyo Night Moon'
+config.color_scheme = 'nord'
+config.window_background_opacity = 0.8
 
 config.font = wezterm.font_with_fallback {
+    'JetBrainsMono Nerd Font',
     'Fira Code Nerd Font',
-    -- 'JetBrainsMono Nerd Font',
 }
 
-config.font_size = 14
+config.font_size = 15
 config.enable_tab_bar = false
 config.window_padding = {
-    left = 8,
-    right = 8,
-    top = 8,
-    bottom = 8,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
+
 config.window_close_confirmation = 'NeverPrompt'
 return config
