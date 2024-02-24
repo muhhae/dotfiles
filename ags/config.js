28a3bf62a3b17d8cb86d48f7b70f4a4c58608290
muhhae <haekal.muhamat@gmail.com>
2024-02-24 09:56:02 +0700

    just trying ags

--- ags/config.js
+++ ags/config.js
@@ -1,1 +1,15 @@
+const entry = App.configDir + '/src/main.ts'
+const outdir = '/tmp/ags/js'
 
+try {
+    await Utils.execAsync([
+        'bun', 'build', entry,
+        '--outdir', outdir,
+        '--external', 'resource://*',
+        '--external', 'gi://*',
+    ])
+} catch (error) {
+    console.error(error)
+}
+const main = await import(`file://${outdir}/main.js`)
+export default main.default
