0cc2820049a17489132619a1793e58d4d7e4eb7b
muhhae <haekal.muhamat@gmail.com>
2024-02-24 15:21:25 +0700

    lsp format use black for python instead of autopep

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -1,9 +1,23 @@
 local autocmd_group = vim.api.nvim_create_augroup("Custom auto-commands", { clear = true })
 
 local custom_format = function()
-    if vim.bo.filetype == 'go' then return end
     vim.cmd('undojoin')
-    if vim.bo.filetype == "templ" then
+    if vim.bo.filetype == 'go' then
+        return
+    elseif vim.bo.filetype == 'python' then
+        print("Using black")
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local cmd = "black " .. vim.fn.shellescape(filename)
+        vim.fn.jobstart(cmd, {
+            on_exit = function()
+                -- reload the buffer only if it's still the current buffer
+                if vim.api.nvim_get_current_buf() == bufnr then
+                    vim.cmd('e!')
+                end
+            end,
+        })
+    elseif vim.bo.filetype == "templ" then
         local bufnr = vim.api.nvim_get_current_buf()
         local filename = vim.api.nvim_buf_get_name(bufnr)
         local cmd = "templ fmt " .. vim.fn.shellescape(filename)
@@ -20,6 +34,7 @@ local custom_format = function()
     end
 end
 
+vim.keymap.set("n", "<leader>f", custom_format)
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     desc = "Auto-format Files after saving",
     callback = custom_format,
@@ -29,7 +44,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     desc = "hide tilde ~",
     callback = function()
-        print("Hiding tilde ~, go to autocmd.lua to disable")
+        -- print("Hiding tilde ~, go to autocmd.lua to disable")
         vim.wo.fillchars = 'eob: '
         vim.cmd('highlight SignColumn guibg=transparent')
     end,
