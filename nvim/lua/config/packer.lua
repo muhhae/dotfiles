a9328cbb06917029821290be130c476654138622
muhhae <haekal.muhamat@gmail.com>
2024-02-27 13:48:05 +0700

    add gitsign

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,6 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+    use 'lewis6991/gitsigns.nvim'
     use 'AlexvZyl/nordic.nvim'
     use({
         "MaximilianLloyd/ascii.nvim",
