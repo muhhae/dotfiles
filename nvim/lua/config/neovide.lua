3831e09548b0e86dcc0dfeca67ceb25084beb95f
muhhae <haekal.muhamat@gmail.com>
2024-02-27 21:17:26 +0700

    tweaks remap so that it copy and paste with system clipboard (i dont like vim register)

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -1,10 +1,5 @@
 vim.o.guifont = "JetBrainsMono Nerd Font:h15"
 
-vim.keymap.set('n', '<C-S-V>', '"+p')    -- Paste normal mode
-vim.keymap.set('v', '<C-S-V>', '"+p')    -- Paste visual mode
-vim.keymap.set('c', '<C-S-V>', '<C-R>+') -- Paste command mode
-vim.keymap.set('i', '<C-S-V>', '<C-R>+') -- Paste command mode
-
 vim.g.neovide_padding_top = 8
 vim.g.neovide_padding_bottom = 8
 vim.g.neovide_padding_right = 8
