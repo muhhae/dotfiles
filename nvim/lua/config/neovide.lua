e462d578b47a794dac6562afff3c42ba313e7713
muhhae <haekal.muhamat@gmail.com>
2024-02-27 13:16:00 +0700

    neovide config

---
+++ nvim/lua/config/neovide.lua
@@ -0,0 +1,8 @@
+vim.o.guifont = "JetBrainsMono Nerd Font:h15"
+vim.keymap.set("i", "<C-S-V>", "<C-R>+")
+vim.g.neovide_padding_top = 8
+vim.g.neovide_padding_bottom = 8
+vim.g.neovide_padding_right = 8
+vim.g.neovide_padding_left = 8
+
+vim.g.neovide_transparency = 0.8
