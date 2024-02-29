c3c4e80208beedb09c31308f86ba5d2fb788f9cf
muhhae <haekal.muhamat@gmail.com>
2024-02-29 21:05:15 +0700

    tweaks waybar

--- waybar/get_spotify_status.sh
+++ waybar/get_spotify_status.sh
@@ -14,7 +14,7 @@ PLAYER="spotify"
 # Format of the information displayed
 # Eg. {{ artist }} - {{ album }} - {{ title }}
 # See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
-FORMAT="{{ title }} - {{ artist }}"
+FORMAT="{{ title }}      {{ artist }}"
 
 # Sends $2 as message to all polybar PIDs that are part of $1
 update_hooks() {
