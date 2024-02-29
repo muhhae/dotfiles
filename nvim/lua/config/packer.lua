34164188a9b6a50c1726dd11f4d30cbbedbc0dc4
muhhae <haekal.muhamat@gmail.com>
2024-02-29 13:03:14 +0700

    adding md preview

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,6 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+    use 'iamcco/markdown-preview.nvim'
     use 'nvim-pack/nvim-spectre'
     use 'rmagatti/auto-session'
     use 'lewis6991/gitsigns.nvim'
