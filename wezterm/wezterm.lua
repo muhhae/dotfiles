257f5f54667b48bf037a836c1b1fec3a3b1f99f2
muhhae <haekal.muhamat@gmail.com>
2024-02-29 20:38:30 +0700

    restructure colorscheme and changing it to catppuccin mocha

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,12 +1,13 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
-config.color_scheme = 'nord'
-config.window_background_opacity = 0.7
+config.color_scheme = 'catppuccin-mocha'
+config.window_background_opacity = 0.9
 
 config.font = wezterm.font_with_fallback {
     'JetBrainsMono Nerd Font',
     'Fira Code Nerd Font',
 }
+
 config.default_cursor_style = 'SteadyBar'
 config.font_size = 15
 config.enable_tab_bar = false
