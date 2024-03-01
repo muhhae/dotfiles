f17084874a67224ac406ca8c621d26b915a8600c
muhhae <haekal.muhamat@gmail.com>
2024-03-01 13:17:44 +0700

    storing personal zsh themes to dotfiles

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 DIR=$(dirname "$0")
 
-ln -sr  $DIR/ags $HOME/.config/.
-ln -sr  $DIR/hypr $HOME/.config/.
-ln -sr  $DIR/nvim $HOME/.config/.
-ln -sr  $DIR/swaync $HOME/.config/.
-ln -sr  $DIR/waybar $HOME/.config/.
-ln -sr  $DIR/wezterm $HOME/.config/.
-ln -sr  $DIR/xdg-desktop-portal $HOME/.config/.
-ln -sr  $DIR/wofi $HOME/.config/.
-ln -sr  $DIR/.zshrc $HOME/.
-ln -sr  $DIR/alacritty $HOME/.config/.
-ln -sr  $DIR/networkmanager-dmenu $HOME/.config/.
-
+ln -sr $DIR/ags $HOME/.config/.
+ln -sr $DIR/hypr $HOME/.config/.
+ln -sr $DIR/nvim $HOME/.config/.
+ln -sr $DIR/swaync $HOME/.config/.
+ln -sr $DIR/waybar $HOME/.config/.
+ln -sr $DIR/wezterm $HOME/.config/.
+ln -sr $DIR/xdg-desktop-portal $HOME/.config/.
+ln -sr $DIR/wofi $HOME/.config/.
+ln -sr $DIR/.zshrc $HOME/.
+ln -sr $DIR/alacritty $HOME/.config/.
+ln -sr $DIR/networkmanager-dmenu $HOME/.config/.
+ln -sr $DIR/zsh-theme/* $HOME/.oh-my-zsh/themes/
