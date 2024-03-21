c4cbfdd96b4b2327e0879d60a9196083990e0a1a
muhhae <haekal.muhamat@gmail.com>
2024-03-22 04:58:00 +0700

    just another tweaks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,29 +1,24 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.color_scheme = 'catppuccin-mocha'
+config.color_scheme = "catppuccin-mocha"
 config.window_background_opacity = 0.9
 
-config.font = wezterm.font_with_fallback {
-    'JetBrainsMono Nerd Font',
-    'Fira Code Nerd Font',
-}
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	"Fira Code Nerd Font",
+})
 
 config.enable_wayland = true
-config.default_cursor_style = 'SteadyBar'
+config.default_cursor_style = "SteadyBar"
 config.font_size = 15
 config.enable_tab_bar = false
 config.window_padding = {
-    left = 6,
-    right = 6,
-    top = 6,
-    bottom = 6,
+	left = 6,
+	right = 6,
+	top = 6,
+	bottom = 6,
 }
 
--- local gpus = wezterm.gui.enumerate_gpus()
--- config.webgpu_preferred_adapter = gpus[1]
-
--- config.front_end = "WebGpu"
--- config.webgpu_power_preference = "LowPower"
-
-config.window_close_confirmation = 'NeverPrompt'
+config.front_end = "WebGpu"
+config.window_close_confirmation = "NeverPrompt"
 return config
