984a3c3dd1729a86d65922b846852e2f3927b374
muhhae <haekal.muhamat@gmail.com>
2024-03-28 14:27:14 +0700

    just another tweaks (i forgot)

--- scripts/killhypr.sh
+++ scripts/killhypr.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 if pgrep -x Hyprland >/dev/null; then
+    pkill swaync
+    pkill rambox
+    pkill discord
+    pkill spotify
+    pkill waybar
     hyprctl dispatch exit 0
     sleep 2
     if pgrep -x Hyprland >/dev/null; then
