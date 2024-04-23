d7cd76533f8978007935e55d5ff4932806c374ab
muhhae <haekal.muhamat@gmail.com>
2024-04-23 20:42:57 +0700

    just wanna commit it coz, i may reinstall my distro

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,9 +1,10 @@
-vim.cmd("colorscheme tokyonight-moon")
--- vim.cmd("colorscheme nord")
--- vim.cmd("colorscheme tokyonight-storm")
-vim.cmd("colorscheme catppuccin-macchiato")
-vim.cmd("colorscheme catppuccin-mocha")
--- vim.cmd("colorscheme nordic")
+-- vim.cmd.colorscheme("tokyonight-moon")
+-- vim.cmd.colorscheme("nord")
+-- vim.cmd.colorscheme("tokyonight-storm")
+vim.cmd.colorscheme("catppuccin-macchiato")
+vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("night-owl")
+-- vim.cmd.colorscheme("nordic")
 
 if vim.g.neovide ~= true then
 	vim.cmd([[highlight Normal guibg=none]])
