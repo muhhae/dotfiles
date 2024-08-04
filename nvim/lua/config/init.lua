c20797dfb8be35a17aa9368441ebfdf3df4a70ee
muhhae <haekal.muhamat@gmail.com>
2024-08-05 06:51:45 +0700

    random tweak

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -4,5 +4,8 @@ require("config.set")
 require("config.autocmd")
 
 if vim.g.neovide == true then
-    require("config.neovide")
+	require("config.neovide")
 end
+
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
