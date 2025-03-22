17086d99e932db40753215e1afd90859bed1e068
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-03-22 22:20:14 +0700

    update

--- setup.sh
+++ setup.sh
@@ -5,4 +5,3 @@ DIR=$(dirname "$0")
 find "$DIR/etc/zsh-theme" -maxdepth 1 -type f -exec ln -srf {} "$HOME/.oh-my-zsh/themes/" \;
 find "$DIR/home-dir" -maxdepth 1 -type f -exec ln -srf {} "$HOME/" \;
 find "$DIR/config-dir" -maxdepth 1 -type f -exec ln -srf {} "$HOME/.config/" \;
-
